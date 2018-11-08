c2eec4070ba0f2876ba33f602e5c033bbfad8bc2
rlfriedman <rlfriedman@google.com>
2018-11-07 23:09:10 -0800

    Updates

---
+++ precache-manifest.fb013c72995f8192d3de0109f9364383.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd77b71a43c3d17f6496e5ed9f16bd2b",
+    "url": "/color-picker-tool/static/media/sunset.dd77b71a.jpg"
+  },
+  {
+    "revision": "31543aa1cdabfa556bf78d427bf24244",
+    "url": "/color-picker-tool/static/media/parrots.31543aa1.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/color-picker-tool/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a6b028d20eca6405f98c65d3f69ba2fb",
+    "url": "/color-picker-tool/static/media/island.a6b028d2.jpg"
+  },
+  {
+    "revision": "5a0c1aa2937ce54b81df5c7adceda8a6",
+    "url": "/color-picker-tool/static/media/flowers.5a0c1aa2.jpg"
+  },
+  {
+    "revision": "79e784c1bc0980cb004f",
+    "url": "/color-picker-tool/static/js/runtime~main.79e784c1.js"
+  },
+  {
+    "revision": "c05153aac915a47aa5b7",
+    "url": "/color-picker-tool/static/js/main.c05153aa.chunk.js"
+  },
+  {
+    "revision": "b969c0d1433ea42d3e18",
+    "url": "/color-picker-tool/static/js/1.b969c0d1.chunk.js"
+  },
+  {
+    "revision": "c05153aac915a47aa5b7",
+    "url": "/color-picker-tool/static/css/main.23e272bf.chunk.css"
+  },
+  {
+    "revision": "f3f0e874c80ce059cb2ce5e432ba6ee8",
+    "url": "/color-picker-tool/index.html"
+  }
+];
